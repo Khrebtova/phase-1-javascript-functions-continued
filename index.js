eb9c2a1d5349bdb8ca6e990886ee7274fd55e71d
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2021-11-29 19:47:57 -0800

    Comleted issingment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun('hike');
+
+function mondayWork(activity= 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+function wrapAdjective(highlight = "*"){
+   return function(parametr1 = 'special'){
+       return `You are ${highlight}${parametr1}${highlight}!`;
+   } 
+}
+wrapAdjective('||')('a dedicated programmer');
